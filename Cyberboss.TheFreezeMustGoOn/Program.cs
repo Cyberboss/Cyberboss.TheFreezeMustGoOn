94b95b916e9a6241824d55d8098d035682fd1657
Cyberboss <Cyberboss@users.noreply.github.com>
2018-04-08 09:16:36 -0400

    Write the script

--- Cyberboss.TheFreezeMustGoOn/Program.cs
+++ Cyberboss.TheFreezeMustGoOn/Program.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Octokit;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
 
 namespace Cyberboss.TheFreezeMustGoOn
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	static class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var ghc = new GitHubClient(new ProductHeaderValue(Assembly.GetExecutingAssembly().GetName().Name))
+			{
+				Credentials = new Credentials(args.First())
+			};
+			var pr = await ghc.PullRequest.Get("tgstation", "tgstation", 36960).ConfigureAwait(false);
+			if (pr.State.Value != ItemState.Open)
+				return;
+			var timeSinceCreated = DateTimeOffset.Now - pr.CreatedAt;
+			var waitFor = new TimeSpan(7, 0, 0, 0) - timeSinceCreated;
+			await Task.Delay(waitFor).ConfigureAwait(false);
+			await ghc.PullRequest.Update(pr.Base.Repository.Id, pr.Number, new PullRequestUpdate { State = ItemState.Closed }).ConfigureAwait(false);
+		}
+	}
 }
